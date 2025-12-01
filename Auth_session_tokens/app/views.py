6bcf8ddd4123844157b121cd935ddcd4259d6d18
Lavya <lavya203@gmail.com>
2025-12-02 02:11:53 +0530

    completed a baic auth system using session tokens

--- Auth_session_tokens/app/views.py
+++ Auth_session_tokens/app/views.py
@@ -4,10 +4,33 @@ from django.contrib.auth import login , authenticate , logout
 from django.contrib.auth.decorators import login_required
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username , password=password)
+        if user:
+            login(request , user)
+            return redirect("/dashboard/")
+        else:
+            return redirect(request , "login.html",{"error":"Invalid credentials"})
+        
+    return redirect(request , "login.html") 
+
 
 def logout(request):
-    pass
+    logout(request)
+    return redirect("/login/")
 
 def signup(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        User.objects.create_user(username=username , password=password)
+        return redirect("/login/")
+    return redirect(request , "signup.html")
+
+@login_required(login_url="/login/")
+def dashboard(request):
+    return redirect(request, "dashboard.html",{"user":request.user})
