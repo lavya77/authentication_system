8c876087c52a98a69b6e5c9fe9f5111dc9fa1365
Lavya <lavya203@gmail.com>
2025-12-02 01:45:04 +0530

    added frontend side of app in templates

--- Auth_session_tokens/app/views.py
+++ Auth_session_tokens/app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login , authenticate , logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def signup(request):
+    pass
